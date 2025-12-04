d5a8c05c6bd25a2a3e2329b37bc72558cd31bfa7
Indrajit <indrajit@Indrajits-MacBook-Pro.local>
2025-12-04 10:17:10 +0530

    changed logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <TopBar />
       {/* Example card */}
-      <div className="bg-card flex flex-col gap-4 shadow-lg rounded-xl p-6 max-w-xl border border-orange-400">
+      <div className="bg-card flex flex-col gap-4 shadow-lg rounded-xl p-6 max-w-xl border border-lime-400">
         <div className="text-2xl lg:text-3xl font-medium">Example Title</div>
         <Separator />
         <div className="text-muted-foreground font-secondary leading-9 line-clamp-8">
