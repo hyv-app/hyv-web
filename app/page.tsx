03f93e79dffcc20def921a9ac323c7fd34d543c6
Indrajit Kutuva <indrajitvijayakumar@gmail.com>
2025-12-09 16:30:16 +0530

    added glowing input box

--- app/page.tsx
+++ app/page.tsx
@@ -16,23 +16,31 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* Input */}
-      <div className="p-px rounded-full w-[calc(100vw-2rem)] max-w-2xl self-center bottom-4 lg:bottom-6 bg-linear-to-r from-violet-400 via-cyan-400 to-lime-400">
+      {/* Glow Input */}
+      <div className="relative w-[calc(100vw-2rem)] max-w-2xl self-center">
+        <div className="absolute inset-0 -z-10 blur-lg bg-linear-to-br from-violet-200 via-cyan-200 to-lime-200" />
         <Input
           placeholder="Ask anything..."
-          className="p-6 font-secondary w-full border-none focus-visible:ring-offset-0 focus-visible:ring-0"
+          className="p-6 font-secondary bg-background rounded-full focus-visible:ring-offset-0 focus-visible:ring-0 shadow-none"
         />
       </div>
 
       {/* Example card */}
-      <div className="bg-card flex flex-col gap-4 shadow-lg rounded-xl p-6 max-w-xl border">
+      <div className="bg-card flex flex-col gap-4 shadow-lg rounded-xl p-6 max-w-xl border border-violet-400">
         <div className="text-2xl lg:text-3xl font-medium">Example Title</div>
         <div className="font-secondary leading-9 line-clamp-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua.
         </div>
       </div>
 
-      <div className="h-dvh bg-neutral-100 flex items-center justify-center rounded-xl">Sample content</div>
+      <div className="h-dvh bg-neutral-100 flex items-center justify-center rounded-xl">
+        Sample content
+      </div>
     </>
   );
 };
