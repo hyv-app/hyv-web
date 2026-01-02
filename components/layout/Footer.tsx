ef2ed432d89ef72e2c37c1eed4fdc1632f607a36
Indrajit <indrajit@Indrajits-MacBook-Pro.local>
2026-01-03 00:23:53 +0530

    styling changes in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -15,24 +15,26 @@ const Footer = () => {
     // }
 
     return (
-        <div className="lg:sticky z-0 bottom-0 w-full max-w-[1440px] mx-auto h-80 flex justify-center items-center px-4 pt-4 lg:px-6 lg:pt-6">
+        <div className="lg:sticky z-0 bottom-0 w-full max-w-[1440px] mx-auto h-80 sm:h-96 flex justify-center items-center px-4 pt-4 lg:px-6 lg:pt-6">
             <div className="relative overflow-hidden w-full h-full flex justify-end text-right items-start">
                 <div className="flex flex-row space-x-12 sm:space-x-16 md:space-x-24 text-base md:text-lg font-secondary">
-                    <ul>
+                    <ul className="flex flex-col gap-1.5">
                         <li>Created with ❤️</li>
-                        <li className="inline-flex gap-2">by
+                        <li className="inline-flex gap-1">by
                             <Link href="https://indrajitvijayakumar.in" target="_blank" className="underline">
                                 Indrajit
                             </Link>
                         </li>
                     </ul>
-                    <ul>
+                    <ul className="flex flex-col gap-1.5 items-end">
                         <li className="hover:underline cursor-pointer w-fit"><Link href="/about">{`What's ${APP_NAME}?`}</Link></li>
+                        <li className="hover:underline cursor-pointer w-fit">Contact</li>
+                        <li className="hover:underline cursor-pointer w-fit">Privacy Policy</li>
                     </ul>
                 </div>
-                <h2 className="absolute bottom-0 left-0 translate-y-1/6 text-[156px] sm:text-[192px] bg-linear-to-r from-violet-400 via-cyan-400 to-lime-400 bg-clip-text text-transparent">
+                <div className="absolute bottom-0 -left-2 translate-y-1/6 text-[156px] sm:text-[192px] bg-linear-to-r from-violet-400 via-cyan-400 to-lime-400 bg-clip-text text-transparent">
                     {APP_NAME}
-                </h2>
+                </div>
             </div>
         </div>
     );
