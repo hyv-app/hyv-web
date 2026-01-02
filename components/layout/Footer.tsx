364fd18c6ba261d9632001970cf0e8064cb35d73
Indrajit K V <43838718+thisisindrajit@users.noreply.github.com>
2026-01-03 00:28:27 +0530

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <div className="lg:sticky z-0 bottom-0 w-full max-w-[1440px] mx-auto h-80 sm:h-96 flex justify-center items-center px-4 pt-4 lg:px-6 lg:pt-6">
             <div className="relative overflow-hidden w-full h-full flex justify-end text-right items-start">
                 <div className="flex flex-row space-x-12 sm:space-x-16 md:space-x-24 text-base md:text-lg font-secondary">
-                    <ul className="flex flex-col gap-1.5">
+                    <ul className="flex flex-col gap-2">
                         <li>Created with ❤️</li>
                         <li className="inline-flex gap-1">by
                             <Link href="https://indrajitvijayakumar.in" target="_blank" className="underline">
@@ -26,7 +26,7 @@ const Footer = () => {
                             </Link>
                         </li>
                     </ul>
-                    <ul className="flex flex-col gap-1.5 items-end">
+                    <ul className="flex flex-col gap-2 items-end">
                         <li className="hover:underline cursor-pointer w-fit"><Link href="/about">{`What's ${APP_NAME}?`}</Link></li>
                         <li className="hover:underline cursor-pointer w-fit">Contact</li>
                         <li className="hover:underline cursor-pointer w-fit">Privacy Policy</li>
